0daac69bcc8ee8c78200500409e1bc6f2c317b83
Oleh Chenusha <oleh.chenusha@zxteam.net>
2022-02-10 00:52:40 +0200

    version 1.01

--- docker-repotool-list.py
+++ docker-repotool-list.py
@@ -5,38 +5,38 @@ import requests
 from requests.auth import HTTPBasicAuth
 import sys
 import os
+import json
 
 class CommunicationException(Exception):
     def __init__(self, message):
         self.message = message
 
-dockerUrl = os.environ.get('URL', None)
-credentialsLogin = os.environ.get('USER', None)
-credentialsPassword = os.environ.get('PASSWORD', None)
+docker_url = os.environ.get('URL', None)
+credentials_login = os.environ.get('USER', None)
+credentials_password = os.environ.get('PASSWORD', None)
 
-credentials = HTTPBasicAuth(credentialsLogin, credentialsPassword)
-getRepositoryCatalog = "%s/v2/_catalog" % dockerUrl
-getRepositoryCatalogResponse = requests.get(getRepositoryCatalog, auth = credentials)
+credentials = HTTPBasicAuth(credentials_login, credentials_password)
+getRepositoryCatalogUrl = "%s/v2/_catalog" % docker_url
+getRepositoryCatalogResponse = requests.get(getRepositoryCatalogUrl, auth = credentials)
 if getRepositoryCatalogResponse.status_code != 200:
     raise CommunicationException("Unexpected response status code: %s" % getRepositoryCatalogResponse.status_code)
 getRepositoryCatalogResponseList = getRepositoryCatalogResponse.json()
 
 for path in getRepositoryCatalogResponseList['repositories']:
-    pathSplit = path.split('/')
-    pathq = pathSplit[-1]
-    getRepositoryUrl = "%s/v2/%s/tags/list" % (dockerUrl, path)
-    getRepositorySnapshotResponse = requests.get(getRepositoryUrl, auth = credentials)
-    if getRepositorySnapshotResponse.status_code != 200:
-        raise CommunicationException("Unexpected response status code: %s" % getRepositorySnapshotResponse.status_code)
-    getRepositorySnapshotResponseList = getRepositorySnapshotResponse.json()
+    getRepositorySnapshotsUrl = "%s/v2/%s/tags/list" % (docker_url, path)
+    getRepositorySnapshotsResponse = requests.get(getRepositorySnapshotsUrl, auth = credentials)
+    if getRepositorySnapshotsResponse.status_code != 200:
+        raise CommunicationException("Unexpected response status code: %s" % getRepositorySnapshotsResponse.status_code)
+    getRepositorySnapshotsResponseList = getRepositorySnapshotsResponse.json()
 
-    for key in getRepositorySnapshotResponseList['tags']:
-        getSnapshotsInfoUrl = '%s/v2/%s/manifests/%s' % (dockerUrl, path, key)
+    for snapshot_key in getRepositorySnapshotsResponseList['tags']:
+        getSnapshotsInfoUrl = '%s/v2/%s/manifests/%s' % (docker_url, path, snapshot_key)
         getSnapshotsInfoResponse = requests.get(getSnapshotsInfoUrl, auth = credentials)
         if getSnapshotsInfoResponse.status_code != 200:
             raise CommunicationException("Unexpected response status code: %s" % getSnapshotsInfoResponse.status_code)
         getSnapshotsInfo = getSnapshotsInfoResponse.json()
-        getSnapshotsInfoList = getSnapshotsInfo['history'][0]['v1Compatibility'].split('"')
-        date = getSnapshotsInfoList[3]
-        print("%s/%s %s" % (path, key, date))
+        inner_json_v1_compatibility_str = getSnapshotsInfo['history'][0]['v1Compatibility']
+        inner_json_v1_compatibility = json.loads(inner_json_v1_compatibility_str)
+        created = inner_json_v1_compatibility['created']
+        print("%s/%s %s" % (path, snapshot_key, created))
         sys.stdout.flush()
